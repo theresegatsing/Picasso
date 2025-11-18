6eae218622d002eea2c07d18bfd619e71ec89591
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-17 22:32:40 -0500

    Updated tan analyzer

--- src/picasso/parser/TanAnalyzer.java
+++ src/picasso/parser/TanAnalyzer.java
@@ -11,9 +11,7 @@ public class TanAnalyzer extends UnaryFunctionAnalyzer {
 	
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Need to remove the floor token
-		// the parameter is the next token(s) on the stack.
-		// But, it needs to be processed
+		tokens.pop(); 
 		ExpressionTreeNode paramETN = SemanticAnalyzer.getInstance().generateExpressionTree(
 				tokens);
 		return new Tangent(paramETN);
