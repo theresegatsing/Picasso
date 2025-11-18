e1fa7aeca99a1c4d562ff88fae5851078f1d9d2c
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-17 19:38:04 -0500

    Implementing Tangent.java

--- src/picasso/parser/language/expressions/Tangent.java
+++ src/picasso/parser/language/expressions/Tangent.java
@@ -1,5 +1,32 @@
 package picasso.parser.language.expressions;
 
-public class Tangent {
+import picasso.parser.language.ExpressionTreeNode;
+
+public class Tangent extends UnaryFunction {
+	
+	
+	/**
+	 * @param param
+	 */
+	public Tangent(ExpressionTreeNode param) {
+		super(param);
+	}
+	
+	
+	/**
+	 * @param x
+	 * @param y
+	 * @return
+	 * @see picasso.parser.language.ExpressionTreeNode#evaluate(double, double)
+	 */
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor result = param.evaluate(x, y);
+		double red = Math.tan(result.getRed());
+		double green = Math.tan(result.getGreen());
+		double blue = Math.tan(result.getBlue());
+
+		return new RGBColor(red, green, blue);
+	}
 
 }
