870965036a0c4cb2e8c6532fcb00aab9f4c0e6e8
menilikdeneke <ymenilik@gmail.com>
2025-11-20 21:34:52 -0500

    Added unit tests for clamp

---
+++ src/tests/ClampTests.java
@@ -0,0 +1,133 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import picasso.parser.ExpressionTreeGenerator;
+import picasso.parser.SemanticAnalyzer;
+import picasso.parser.Tokenizer;
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Clamp;
+import picasso.parser.language.expressions.Floor;
+import picasso.parser.language.expressions.Plus;
+import picasso.parser.language.expressions.RGBColor;
+import picasso.parser.language.expressions.X;
+import picasso.parser.language.expressions.Y;
+import picasso.parser.tokens.IdentifierToken;
+import picasso.parser.tokens.Token;
+import picasso.parser.tokens.chars.LeftParenToken;
+import picasso.parser.tokens.chars.RightParenToken;
+import picasso.parser.tokens.functions.ClampToken;
+
+/**
+ * This organization doesn't take advantage of the @BeforeEach set up step, but
+ * it may be easier to think about the tests this way. (Duplicates some tests
+ * from other JUnit Test Classes.)
+ * 
+ * @author Menilik Deneke
+ */
+class ClampTests {
+
+	private static ExpressionTreeGenerator parser;
+	private Tokenizer tokenizer;
+
+	/**
+	 * Just need to get the semantic analyzer once. Calling getInstance again
+	 * doesn't get a new/fresh instance.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		parser = new ExpressionTreeGenerator();
+	}
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		tokenizer = new Tokenizer();
+	}
+
+	@Test
+	public void testClampEvaluation() {
+		Clamp myTree = new Clamp(new X());
+
+		// some straightforward tests
+		assertEquals(new RGBColor(1, 1, 1), myTree.evaluate(3, 3));
+		assertEquals(new RGBColor(-1, -1, -1), myTree.evaluate(-3, -3));
+		assertEquals(new RGBColor(-0.5, -0.5, -0.5), myTree.evaluate(-.5, -.5));
+
+		// test the ints; remember that y's value doesn't matter
+		for (int i = -1; i <= 1; i++) {
+			assertEquals(new RGBColor(i, i, i), myTree.evaluate(i, -i));
+			assertEquals(new RGBColor(i, i, i), myTree.evaluate(i, i));
+		}
+
+		double[] tests = { -.7, -.00001, .000001, .5 };
+
+		for (double testVal : tests) {
+			testVal = 2.5;
+			double clampOfTestVal = 1.0; // because 2.5 clamps to 1
+			assertEquals(new RGBColor(clampOfTestVal, clampOfTestVal, clampOfTestVal), myTree.evaluate(testVal, -1));
+			assertEquals(new RGBColor(clampOfTestVal, clampOfTestVal, clampOfTestVal),
+					myTree.evaluate(testVal, testVal));
+		}
+	}
+
+	@Test
+	public void clampFunctionTests() {
+		ExpressionTreeNode e = parser.makeExpression("clamp( x )");
+		assertEquals(new Clamp(new X()), e);
+
+		e = parser.makeExpression("clamp( x + y )");
+		assertEquals(new Clamp(new Plus(new X(), new Y())), e);
+	}
+
+	@Test
+	public void testToString() {
+		ExpressionTreeNode clamp = new Clamp(new Y());
+		assertEquals("Clamp(y)", clamp.toString());
+	}
+
+	@Test
+	public void testEquals() {
+		ExpressionTreeNode clamp = new Clamp(new X());
+		ExpressionTreeNode clampSame = new Clamp(new X());
+		ExpressionTreeNode clampDifferent = new Clamp(new Y());
+
+		assertEquals(clamp, clamp);
+		assertEquals(clampSame, clamp);
+
+		assertNotEquals(clamp, clampDifferent);
+		assertNotEquals(clampDifferent, clampSame);
+		
+		ExpressionTreeNode floor = new Floor(new X());
+		assertNotEquals(
+			    clamp.evaluate(0.7, 0),  // Clamp(0.7) = 0.7
+			    floor.evaluate(0.7, 0)   // Floor(0.7) = 0
+			);
+
+		assertNotEquals(
+			   clamp.evaluate(-0.3, 0),  // Clamp(-0.3) = -0.3  
+			   floor.evaluate(-0.3, 0)   // Floor(-0.3) = -1
+			);
+	}
+
+	@Test
+	public void testTokenizeBasicFunctionExpression() {
+		String expression = "clamp(x)";
+		List<Token> tokens = tokenizer.parseTokens(expression);
+		assertEquals(new ClampToken(), tokens.get(0));
+		assertEquals(new LeftParenToken(), tokens.get(1));
+		assertEquals(new IdentifierToken("x"), tokens.get(2));
+		assertEquals(new RightParenToken(), tokens.get(3));
+	}
+
+}
