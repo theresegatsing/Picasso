357207d9125f0d96da0eef6ac1f588a02e092cd0
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-12-02 15:41:47 -0500

    Implemented the cos token

---
+++ src/picasso/parser/tokens/functions/CosToken.java
@@ -0,0 +1,14 @@
+package picasso.parser.tokens.functions;
+
+/**
+ *  Represents the Sin function token
+ * 
+ * @author Therese Elvira Mombou Gatsing
+ */
+public class CosToken extends FunctionToken {
+	
+	public CosToken() {
+		super("Cos Function Token");
+	}
+
+}
