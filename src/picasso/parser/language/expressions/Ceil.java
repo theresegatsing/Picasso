dc103ef63ea4e0b677f901f1d8472bcd7f41639c
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-12-02 14:24:06 -0500

    Implemented the ceil class

---
+++ src/picasso/parser/language/expressions/Ceil.java
@@ -0,0 +1,38 @@
+package picasso.parser.language.expressions;
+
+import picasso.parser.language.ExpressionTreeNode;
+
+/**
+ * Represents the Ceil function in the Picasso language.
+ * 
+ * @author Therese Elvira Mombou Gatsing
+ * 
+ */
+public class Ceil extends UnaryFunction {
+
+	/**
+	 * Create a ceil expression tree that takes as a parameter the given expression
+	 * 
+	 * @param param the expression to ceil
+	 */
+	public Ceil(ExpressionTreeNode param) {
+		super(param);
+	}
+
+	/**
+	 * Evaluates this expression at the given x,y point by evaluating the ceil of
+	 * the function's parameter.
+	 * 
+	 * @return the color from evaluating the ceil of the expression's parameter
+	 */
+	@Override
+	public RGBColor evaluate(double x, double y) {
+		RGBColor result = param.evaluate(x, y);
+		double red = Math.ceil(result.getRed());
+		double green = Math.ceil(result.getGreen());
+		double blue = Math.ceil(result.getBlue());
+
+		return new RGBColor(red, green, blue);
+	}
+
+}
