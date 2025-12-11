65f20734a20c6504ac738766e8446677bb7cb372
menilikdeneke <ymenilik@gmail.com>
2025-12-10 22:44:13 -0500

    mandelbrot changes

---
+++ src/picasso/parser/tokens/functions/MandelbrotToken.java
@@ -0,0 +1,13 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * Represents the Mandelbrot function token
+ * @author Menilik Deneke
+ */
+
+public class MandelbrotToken extends FunctionToken {
+	
+	public MandelbrotToken() {
+		super("Mandelbrot Function Token");
+	}
+}
